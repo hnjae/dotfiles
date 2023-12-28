c78f0ec42c0834ae61c1b9c4bc6451ad19b734d5
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2023-12-28 15:38:22 +0900

    fix(lualine): null-ls 가 비활성화 되었을때도 출력 되었던 버그 수정

--- nvim/lua/plugins/lualine/components/lsp.lua
+++ nvim/lua/plugins/lualine/components/lsp.lua
@@ -1,35 +1,26 @@
-local get_clients = function()
-  local clients = vim.lsp.get_active_clients({ bufnr = 0 })
-  local names = {}
-
-  --@type string
-  local client_name
-  for _, client in ipairs(clients) do
-    if client.name == "null-ls" then
-      -- TODO: selene complains but it works <2023-12-14>
-      goto continue
-    end
-
-    client_name = client.name:sub(-16, -1) == "_language_server" and client.name:sub(1, -17) .. "-ls"
-      or (client.name:sub(-3, -1) == "_ls" and client.name:sub(1, -4) .. "-ls" or client.name)
-
-    table.insert(names, client_name)
-    ::continue::
-  end
-
-  return names
-end
-
 return {
   -- Lsp server name .
   function()
-    -- local msg = "No Active LSP"
-    -- get_clients()
-    return table.concat(get_clients(), ", ")
+    local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+    local names = {}
+
+    --@type string
+    local client_name
+    for _, client in ipairs(clients) do
+      if client.name == "null-ls" then
+        -- TODO: selene complains but it works <2023-12-14>
+        goto continue
+      end
+
+      client_name = client.name:sub(-16, -1) == "_language_server" and client.name:sub(1, -17) .. "-ls"
+        or (client.name:sub(-3, -1) == "_ls" and client.name:sub(1, -4) .. "-ls" or client.name)
+
+      table.insert(names, client_name)
+      ::continue::
+    end
+
+    return table.concat(names, ", ")
   end,
   icon = "",
-  cond = function()
-    -- return (next(vim.lsp.get_active_clients()) ~= nil)
-    return (next(get_clients()) ~= nil)
-  end,
+  cond = nil,
 }
--- nvim/lua/plugins/lualine/components/null-ls.lua
+++ nvim/lua/plugins/lualine/components/null-ls.lua
@@ -1,24 +1,25 @@
 local sources = require("null-ls.sources")
 
--- TODO: null-ls 가 vim.lsp.get_active_clients({ bufnr = 0 }) 에 있는지 확인 <2023-12-27>
-local get_sources = function()
-  local names = {}
-  local filetype = vim.api.nvim_buf_get_option(0, "filetype")
-  for _, source in ipairs(sources.get_available(filetype)) do
-    if not names[source.name] then
-      table.insert(names, source.name)
-      names[source.name] = true
-    end
-  end
-  return names
-end
-
 return {
   [1] = function()
-    return table.concat(get_sources(), ", ")
+    local names = {}
+
+    -- null-ls 가 활성화 되어있는지 확인
+    local clients = vim.lsp.get_active_clients({ bufnr = 0, name = "null-ls" })
+    if next(clients) == nil then
+      return ""
+    end
+
+    local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+    for _, source in ipairs(sources.get_available(filetype)) do
+      if not names[source.name] then
+        table.insert(names, source.name)
+        names[source.name] = true
+      end
+    end
+    return table.concat(names, ", ")
   end,
   icon = "󰟢",
-  cond = function()
-    return (next(get_sources()) ~= nil)
-  end,
+  -- NOTE: lualine 은 출력이 없을 경우를 별도로 체크할 필요가 없다. <2023-12-28>
+  cond = nil,
 }
