152626e56bc593ae134173914bd35d376b4b726c
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2023-12-28 11:19:31 +0900

    fix(rest.nvim): exception handling in formatting

--- nvim/lua/plugins/dev-REST-client.lua
+++ nvim/lua/plugins/dev-REST-client.lua
@@ -15,17 +15,20 @@ return {
       show_http_info = true,
       show_headers = true,
       formatters = {
-        json = "jq",
+        json = vim.fn.executable("jq") == 1 and "jq" or "cat",
         html = function(body)
-          return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+          if vim.fn.executable("tidy") == 1 then
+            return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+          end
+          return vim.fn.system({ "echo", "--" }, body)
         end,
       },
     },
   },
   cmd = { "RestLog", "RestSelectEnv" },
   keys = {
-    { [1] = prefix .. "r", [2] = "<Plug>RestNvim", ft = "http", desc = "RestNvim" },
+    { [1] = prefix .. "r", [2] = "<Plug>RestNvim",        ft = "http", desc = "RestNvim" },
     { [1] = prefix .. "R", [2] = "<Plug>RestNvimPreview", ft = "http", desc = "RestNvimPreview" },
-    { [1] = prefix .. "g", [2] = "<cmd>RestLog<CR>", ft = "http", desc = "RestLog" },
+    { [1] = prefix .. "g", [2] = "<cmd>RestLog<CR>",      ft = "http", desc = "RestLog" },
   },
 }
