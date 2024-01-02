956a00f9b9d34e6997b4ca8740c6cabbbae69163
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-01-02 15:30:54 +0900

    feat: migrate from fugitive to neogit

--- nvim/lua/plugins/feature/git.lua
+++ nvim/lua/plugins/feature/git.lua
@@ -9,57 +9,12 @@ return {
   },
   {
     [1] = "TimUntersberger/neogit",
-    enabled = false,
+    enabled = true,
     lazy = true,
     opts = {},
     cmd = {
       "Neogit",
       "NeogitResetState",
     },
-  },
-  {
-    -- NOTE: fugitive does not support git@github.com: syntax <2024-01-02>
-    [1] = "tpope/vim-fugitive",
-    lazy = true,
-    enabled = true,
-    cmd = {
-      "G",
-      "Git",
-      "Ggrep",
-      "Gclog",
-      "Gllog",
-      "Gcd",
-      "Glcd",
-      "Gedit",
-      "Gsplit",
-      "Gvsplit",
-      "Gtabedit",
-      "Gpedit",
-      "Gdrop",
-      "Gread",
-      "Gwrite",
-      "Gwq",
-      "Gdiffsplit",
-      "Gvdiffsplit",
-      "Ghdiffsplit",
-      "GMove",
-      "GRename",
-      "GDelete",
-      "GRemove",
-      "GUnlink",
-      "GBrowse",
-    },
-    config = function()
-      -- remove(delete) all deprecated commands
-      for _, command in pairs({
-        "Gremove",
-        "Gdelete",
-        "Gmove",
-        "Grename",
-        "Gbrowse",
-      }) do
-        vim.api.nvim_del_user_command(command)
-      end
-    end,
   },
 }
