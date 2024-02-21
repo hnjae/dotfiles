359d5ff1a81b8d01509c14d5cfe44dea92900404
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-02-21 16:33:16 +0900

    messages에 filetype 추가

--- nvim/lua/plugins/lib/message.lua
+++ nvim/lua/plugins/lib/message.lua
@@ -7,12 +7,39 @@ return {
   cmd = {
     "Messages",
   },
-  opts = {},
+  opts = function()
+    local ret = {
+      -- help: nvim_open_win()'s `{config}`
+      ---@type fun(lines: string[]): table
+      buffer_opts = function(lines)
+        local gheight = vim.api.nvim_list_uis()[1].height
+        local gwidth = vim.api.nvim_list_uis()[1].width
+        return {
+          title = "Messages",
+          relative = "editor",
+          width = gwidth - 1,
+          height = require("messages.utils").clip_val(8, #lines, gheight * 0.5),
+          anchor = "SW",
+          row = gheight - 1,
+          col = 0,
+          style = "minimal",
+          border = "rounded",
+          zindex = 1,
+        }
+      end,
+      ---@type fun(winnr: number): nil
+      post_open_float = function(winnr)
+        local bufnr = vim.api.nvim_win_get_buf(winnr)
+        vim.api.nvim_buf_set_option(bufnr, "filetype", "txt")
+        -- vim.api.nvim_buf_set_option(bufnr, "wrap", true)
+      end,
+    }
+    return ret
+  end,
   config = function(_, opts)
     require("messages").setup(opts)
-
-    -- Msg = function(...)
-    --   require("messages.api").capture_thing(...)
-    -- end
+    Msg = function(...)
+      require("messages.api").capture_thing(...)
+    end
   end,
 }
