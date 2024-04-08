06845fc563c1d84dce4a0357e2f038c7a3795188
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-04-08 14:34:05 +0900

    fix: change codespell's severity and method

--- nvim/lua/plugins/lsp/configs/all.lua
+++ nvim/lua/plugins/lsp/configs/all.lua
@@ -3,22 +3,31 @@ local M = {}
 
 M.get_null_ls_sources = function(null_ls, _)
   local ret = {
+    -- completion 관련은 cmp에 추가. 여기에 추가하면 LSP 가 제공한 소스로
+    -- 관주되어 우선순위 관리가 까다로움
     -- null_ls.builtins.completion.luasnip,
     -- null_ls.builtins.completion.spell, -- completion scope 더럽힘. 쓰지말자.
     -- null_ls.builtins.completion.tags,
+
     -- null_ls.builtins.hover.printenv,
     -- null_ls.builtins.hover.dictionary,
   }
 
   local mapping = {
-    -- TODO: microsoft windows path  <2024-01-02>
     codespell = {
       null_ls.builtins.diagnostics.codespell.with({
         args = {
-          "-x",
-          vim.fn.stdpath("config") .. "/spell/en.utf-8.add",
+          "-X",
+          require("plenary.path"):new(
+            vim.fn.stdpath("config"),
+            "/spell/en.utf-8.add"
+          ).filename,
           "-",
         },
+        diagnostics_postprocess = function(diagnostic)
+          diagnostic.severity = vim.diagnostic.severity.HINT
+        end,
+        method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
       }),
     },
   }
