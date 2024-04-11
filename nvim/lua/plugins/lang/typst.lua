6d56b76d156dc2b56c77151403eb723d6480f7fe
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-04-11 15:42:22 +0900

    feat: new filetype support: typst

---
+++ nvim/lua/plugins/lang/typst.lua
@@ -0,0 +1,48 @@
+---@type LazySpec[]
+return {
+  {
+    [1] = "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    init = function()
+      require("state.treesitter-langs"):add("typst")
+    end,
+  },
+  {
+    [1] = "kaarmu/typst.vim",
+    lazy = false,
+  },
+  {
+    [1] = "chomosuke/typst-preview.nvim",
+    lazy = true,
+    ft = { "typst" },
+    enabled = false,
+    build = function()
+      require("typst-preview").update()
+    end,
+  },
+  {
+    [1] = "neovim/nvim-lspconfig",
+    optional = true,
+    opts = {
+      servers = {
+        typst_lsp = {
+          ---@class LspconfigSetupOptsSpec
+          settings = {},
+        },
+      },
+    },
+  },
+  {
+    -- :h conform-formatters
+    -- https://github.com/stevearc/conform.nvim/tree/master/lua/conform/formatters
+    [1] = "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        typst = {
+          "typstfmt",
+        },
+      },
+    },
+  },
+}
