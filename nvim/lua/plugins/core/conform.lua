ce6eaadf3856ec283df5bed23306a2a61f0b5297
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-04-13 20:34:42 +0900

    feat(lualine): Add conform component to lualine

--- nvim/lua/plugins/core/conform.lua
+++ nvim/lua/plugins/core/conform.lua
@@ -1,66 +1,157 @@
----@type LazySpec
+---@type LazySpec[]
 return {
-  [1] = "stevearc/conform.nvim",
-  lazy = true,
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = function(_, opts)
-    local ret = {
-      lsp_fallback = true,
-      -- Define your formatters
-      formatters_by_ft = {},
-      -- format_on_save = { timeout_ms = 1000 },
-      format_after_save = { lsp_fallback = true },
-    }
-    return vim.tbl_deep_extend("keep", ret, opts)
-  end,
-  keys = {
-    {
-      [1] = "==",
-      [2] = function()
-        require("conform").format({
-          async = false,
-          timeout_ms = 4000,
-          quiet = false,
-        }, function()
-          local msg = "Format Done"
-          vim.notify(msg, nil, {
-            title = "Conform",
-            timeout = 300,
-            hide_from_history = true,
-            animate = false,
-          })
-        end)
-      end,
-      desc = "Format",
-      mode = "n",
+  {
+    [1] = "stevearc/conform.nvim",
+    lazy = true,
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = function(_, opts)
+      local ret = {
+        lsp_fallback = true,
+        -- Define your formatters
+        formatters_by_ft = {},
+        -- format_on_save = { timeout_ms = 1000 },
+        format_after_save = { lsp_fallback = true },
+      }
+      return vim.tbl_deep_extend("keep", ret, opts)
+    end,
+    keys = {
+      {
+        [1] = "==",
+        [2] = function()
+          require("conform").format({
+            async = false,
+            timeout_ms = 4000,
+            quiet = false,
+          }, function()
+            local msg = "Format Done"
+            vim.notify(msg, nil, {
+              title = "Conform",
+              timeout = 300,
+              hide_from_history = true,
+              animate = false,
+            })
+          end)
+        end,
+        desc = "Format",
+        mode = "n",
+      },
+      -- {
+      --   [1] = "==",
+      --   [2] = function()
+      --     require("conform").format({
+      --       async = false,
+      --       timeout_ms = 4000,
+      --       quiet = false,
+      --     }, function()
+      --       local msg = "Format Done"
+      --       vim.notify(msg, nil, {
+      --         title = "Conform",
+      --         timeout = 300,
+      --         hide_from_history = true,
+      --         animate = false,
+      --       })
+      --     end)
+      --   end,
+      --   desc = "Range-Format",
+      --   mode = "v",
+      -- },
     },
-    -- {
-    --   [1] = "==",
-    --   [2] = function()
-    --     require("conform").format({
-    --       async = false,
-    --       timeout_ms = 4000,
-    --       quiet = false,
-    --     }, function()
-    --       local msg = "Format Done"
-    --       vim.notify(msg, nil, {
-    --         title = "Conform",
-    --         timeout = 300,
-    --         hide_from_history = true,
-    --         animate = false,
-    --       })
-    --     end)
-    --   end,
-    --   desc = "Range-Format",
-    --   mode = "v",
-    -- },
-  },
-  config = function(_, opts)
-    require("conform").setup(opts)
+    config = function(_, opts)
+      require("conform").setup(opts)
 
-    -- If you want the formatexpr, here is the place to set it
-    -- 이게 == 대체할 것 (아마도)
-    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-  end,
+      -- If you want the formatexpr, here is the place to set it
+      -- 이게 == 대체할 것 (아마도)
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+    end,
+  },
+  {
+    [1] = "nvim-lualine/lualine.nvim",
+    optional = true,
+    ---@param opts myLualineOpts
+    opts = function(_, opts)
+      local utils = require("utils")
+      local val = require("val")
+
+      local hide_width = 65
+      local truc_width = 100
+      local num_source_semi_limit = 2
+
+      local icon = utils.enable_icon and (require("val").icons.sort .. " ")
+        or ""
+
+      local modules = require("lualine_require").lazy_require({
+        conform = "conform",
+      })
+
+      local component = {
+        [1] = function()
+          local lualine_width = require("val.plugins.lualine").options.globalstatus
+              and vim.o.columns
+            or vim.fn.winwidth(0)
+
+          if lualine_width < hide_width then
+            return ""
+          end
+
+          local formatters = modules.conform.list_formatters(0)
+
+          local names = {}
+          for _, formatter in ipairs(formatters) do
+            if formatter.available then
+              table.insert(names, formatter.name)
+            end
+          end
+
+          if #names == 0 and modules.conform.will_fallback_lsp() then
+            table.insert(names, "LSP")
+          end
+
+          if #names == 0 then
+            if vim.bo.buftype ~= "" or vim.bo.filetype == "text" then
+              return ""
+            end
+
+            return (
+              require("utils").enable_icon and (icon .. val.icons.empty_set)
+              or "No formatter available"
+            )
+          end
+
+          if lualine_width < truc_width then
+            return (string.format("%s[%s]", icon, #names))
+          end
+
+          if
+            #names > num_source_semi_limit
+            and (#names - num_source_semi_limit) > 1
+          then
+            return string.format(
+              "%s%s +[%s]",
+              icon,
+              table.concat({ unpack(names, 1, num_source_semi_limit) }, ", "),
+              #names - num_source_semi_limit
+            )
+          end
+
+          return string.format("%s%s", icon, table.concat(names, ", "))
+        end,
+        cond = nil,
+      }
+
+      if not opts.sections then
+        opts.sections = {}
+      end
+      if not opts.sections.lualine_x then
+        opts.sections.lualine_x = {}
+      end
+
+      vim.list_extend(opts.sections.lualine_x, {
+        {
+          component = component,
+          priority = 92,
+        },
+      })
+    end,
+  },
 }
--- nvim/lua/plugins/core/none-ls.lua
+++ nvim/lua/plugins/core/none-ls.lua
@@ -7,42 +7,140 @@
 
 local val = require("val")
 
----@type LazySpec
+---@type LazySpec[]
 return {
-  -- "jose-elias-alvarez/null-ls.nvim",
-  [1] = "nvimtools/none-ls.nvim",
-  lazy = true,
-  event = { "VeryLazy" },
-  cmd = { "NullLsInfo", "NullLsLog" },
-  enabled = true,
-  dependencies = {
-    "nvim-lua/plenary.nvim",
+  {
+    -- "jose-elias-alvarez/null-ls.nvim",
+    [1] = "nvimtools/none-ls.nvim",
+    lazy = true,
+    event = { "VeryLazy" },
+    cmd = { "NullLsInfo", "NullLsLog" },
+    enabled = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/CONFIG.md
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
+    opts = function(_, opts)
+      opts = vim.tbl_deep_extend("force", opts, {
+        debug = false,
+        fallback_severity = vim.diagnostic.severity.WARN,
+
+        -- diagnostics_format = "#{m} (#{s})",
+        diagnostics_format = "[#{c}] #{m} (#{s})",
+        -- root_dir = require("null-ls.utils").root_pattern(unpack(val.root_patterns)),
+        sources = {},
+        on_attach = val.on_attach,
+
+        should_attach = function(bufnr)
+          if vim.api.nvim_buf_get_option(bufnr, "buftype") ~= "" then
+            return false
+          end
+          return true
+        end,
+      })
+
+      return opts
+    end,
+    config = function(_, opts)
+      local null_ls = require("null-ls")
+      null_ls.setup(opts)
+    end,
   },
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/CONFIG.md
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTIN_CONFIG.md
-  opts = function(_, opts)
-    opts = vim.tbl_deep_extend("force", opts, {
-      debug = false,
-      fallback_severity = vim.diagnostic.severity.WARN,
+  {
+    [1] = "nvim-lualine/lualine.nvim",
+    optional = true,
+    ---@param opts myLualineOpts
+    opts = function(_, opts)
+      local utils = require("utils")
+      local hide_width = 65
+      local truc_width = 100
+      local num_source_semi_limit = 2
 
-      -- diagnostics_format = "#{m} (#{s})",
-      diagnostics_format = "[#{c}] #{m} (#{s})",
-      -- root_dir = require("null-ls.utils").root_pattern(unpack(val.root_patterns)),
-      sources = {},
-      on_attach = val.on_attach,
+      -- TODO: null-ls 타입(diagnostics, formatter)에 따라 우선 순위로 정렬 <2024-01-10>
+      local suppress_sources = {
+        codespell = true,
+      }
+      local icon = utils.enable_icon and (require("val").icons.null .. " ")
+        or ""
 
-      should_attach = function(bufnr)
-        if vim.api.nvim_buf_get_option(bufnr, "buftype") ~= "" then
-          return false
-        end
-        return true
-      end,
-    })
+      local modules = require("lualine_require").lazy_require({
+        sources = "null-ls.sources",
+      })
 
-    return opts
-  end,
-  config = function(_, opts)
-    local null_ls = require("null-ls")
-    null_ls.setup(opts)
-  end,
+      local component = {
+        [1] = function()
+          local lualine_width = require("val.plugins.lualine").options.globalstatus
+              and vim.o.columns
+            or vim.fn.winwidth(0)
+
+          if lualine_width < hide_width then
+            return ""
+          end
+
+          local clients =
+            vim.lsp.get_active_clients({ bufnr = 0, name = "null-ls" })
+          local is_null_ls = #clients ~= 0
+
+          local names = {}
+          if is_null_ls then
+            for _, source in
+              ipairs(modules.sources.get_available(vim.bo.filetype))
+            do
+              if
+                not names[source.name] and not suppress_sources[source.name]
+              then
+                table.insert(names, source.name)
+                names[source.name] = true
+              end
+            end
+          end
+
+          if next(names) == nil then
+            if vim.bo.buftype ~= "" or vim.bo.filetype == "text" then
+              return ""
+            end
+
+            return (
+              require("utils").enable_icon and (icon .. val.icons.empty_set)
+              or "No soures attached"
+            )
+          end
+
+          if lualine_width < truc_width then
+            return (string.format("%s[%s]", icon, #names))
+          end
+
+          if
+            #names > num_source_semi_limit
+            and (#names - num_source_semi_limit) > 1
+          then
+            return string.format(
+              "%s%s +[%s]",
+              icon,
+              table.concat({ unpack(names, 1, num_source_semi_limit) }, ", "),
+              #names - num_source_semi_limit
+            )
+          end
+
+          return string.format("%s%s", icon, table.concat(names, ", "))
+        end,
+        cond = nil,
+      }
+
+      if not opts.sections then
+        opts.sections = {}
+      end
+      if not opts.sections.lualine_x then
+        opts.sections.lualine_x = {}
+      end
+
+      vim.list_extend(opts.sections.lualine_x, {
+        {
+          component = component,
+          priority = 91,
+        },
+      })
+    end,
+  },
 }
