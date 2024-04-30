66cb1ac09f3c9dd61fed33e3c56567a87d972c41
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-04-30 17:37:28 +0900

    feat: Add key-mapping for gitsigns.nvim

--- nvim/lua/plugins/ui/gitsigns.lua
+++ nvim/lua/plugins/ui/gitsigns.lua
@@ -1,4 +1,7 @@
----@type LazySpec
+local prefix = require("val.prefix")
+local map_keyword = require("val.map-keyword")
+
+---@type LazySpec[]
 return {
   {
     [1] = "lewis6991/gitsigns.nvim",
@@ -6,5 +9,19 @@ return {
     lazy = true,
     event = "VeryLazy",
     opts = {},
+    ---@type LazyKeysSpec[]
+    keys = {
+      -- stylua: ignore start
+      { [1] = prefix.buffer .. map_keyword.git,        [2] = nil,                                 desc = "+git"            },
+      { [1] = prefix.buffer .. map_keyword.git .. "p", [2] = "<cmd>Gitsigns preview_hunk<CR>",    desc = "preview-hunk"    },
+      { [1] = prefix.buffer .. map_keyword.git .. "s", [2] = "<cmd>Gitsigns stage_hunk<CR>",      desc = "stage-hunk"      },
+      { [1] = prefix.buffer .. map_keyword.git .. "u", [2] = "<cmd>Gitsigns undo_stage_hunk<CR>", desc = "undo-stage-hunk" },
+      -- stylua: ignore end
+
+      -- stylua: ignore start
+      { [1] = "]" .. map_keyword.git, [2] = "<cmd>Gitsigns next_hunk<CR>", desc = "next-hunk" },
+      { [1] = "[" .. map_keyword.git, [2] = "<cmd>Gitsigns prev_hunk<CR>", desc = "prev-hunk" },
+      -- stylua: ignore end
+    },
   },
 }
