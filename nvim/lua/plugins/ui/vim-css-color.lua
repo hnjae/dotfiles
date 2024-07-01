c3cd360bc7f8a445dff9ee5bbf914e8d1060aba1
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-01 18:07:33 +0900

    feat: nvim-highlight-colors

--- nvim/lua/plugins/ui/vim-css-color.lua
+++ nvim/lua/plugins/ui/vim-css-color.lua
@@ -1,5 +1,36 @@
+---@type LazySpec[]
 return {
   -- TODO: make following to support *.kdl, *.toml <2023-02-04, Hyunjae Kim>
   -- TODO: check: https://github.com/brenoprata10/nvim-highlight-colors
-  { "ap/vim-css-color" }, -- preview color
+  -- { "ap/vim-css-color" }, -- preview color
+
+  -- {
+  --   [1] = "NvChad/nvim-colorizer.lua",
+  --   enabled = vim.opt.termguicolors:get(),
+  --   lazy = true,
+  --   ft = {
+  --     "css",
+  --   },
+  --   opts = {
+  --     user_default_options = {
+  --       filetypes = {
+  --         "css",
+  --       },
+  --       tailwind = "both",
+  --     },
+  --   },
+  -- },
+
+  {
+    [1] = "brenoprata10/nvim-highlight-colors",
+    cond = vim.opt.termguicolors:get(),
+    lazy = true,
+    event = "VeryLazy",
+    cmd = {
+      "HighlightColors",
+    },
+    opts = {
+      enable_tailwind = true,
+    },
+  },
 }
