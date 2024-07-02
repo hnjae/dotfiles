81fbc49bfecc6122f159ea2f5d6bdaa7abf67616
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-02 17:36:04 +0900

    chore: use system's nixpkgs in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
   description = "devshell";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, gitignore, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in with pkgs; {
-        devShell = mkShell { buildInputs = [
-          selene
-          stylua
-          sumneko-lua-language-server
-        ]; };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; {
+        devShell = mkShell {
+          buildInputs = [
+            selene
+            stylua
+            sumneko-lua-language-server
+          ];
+        };
       });
 }
