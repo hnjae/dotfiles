4caeb9272c48e4b40dece6c314b8d84130ac279c
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-17 14:31:59 +0900

    feat: add new textobjects using treesitter

--- nvim/lua/plugins/editing/treesitter-textobjects.lua
+++ nvim/lua/plugins/editing/treesitter-textobjects.lua
@@ -2,6 +2,7 @@
 return {
   [1] = "nvim-treesitter/nvim-treesitter-textobjects",
   lazy = true,
+  enabled = true,
   cond = require("utils").is_treesitter,
   event = { "VeryLazy" },
   dependencies = {
@@ -9,53 +10,95 @@ return {
   },
   opts = {
     textobjects = {
+      move = {
+        enable = true,
+        set_jumps = true, -- ??
+        goto_next_start = {
+          -- override's vim default (`]m`: 동일 기능 수행)
+          ["]m"] = { query = "@function.outer", desc = "@function.outer" },
+
+          -- override's vim default (`]/`: 동일 기능 수행)
+          ["]/"] = { query = "@comment.outer", desc = "@comment.outer" },
+          ["]*"] = { query = "@comment.outer", desc = "@comment.outer" },
+
+          -- override's vim default
+          ["]]"] = { query = "@class.outer", desc = "@class.outer" },
+
+          -- override's vim default (기존: misspelled)
+          ["]s"] = { query = "@statement.outer", desc = "@statement.outer" },
+        },
+        goto_next_end = {
+          ["]M"] = { query = "@function.outer", desc = "@function.outer" },
+          ["]S"] = { query = "@statement.outer", desc = "@statement.outer" },
+        },
+        goto_previou_start = {
+          ["[m"] = "@function.outer",
+          ["[/"] = { query = "@comment.outer", desc = "@comment.outer" },
+          ["[*"] = { query = "@comment.outer", desc = "@comment.outer" },
+          ["[["] = { query = "@class.outer", desc = "@class.outer" },
+
+          -- override's vim default (기존: misspelled)
+          ["[s"] = { query = "@statement.outer", desc = "@statement.outer" },
+        },
+        goto_previou_end = {
+          ["[M"] = { query = "@function.outer", desc = "@function.outer" },
+          ["[S"] = { query = "@statement.outer", desc = "@statement.outer" },
+        },
+      },
       select = {
         enable = true,
         lookahead = true,
         keymaps = {
-          --[[ NOTE:  <2024-04-02>
+          --[[
+          NOTE:  <2024-04-02>
           reserved keywords:
-          b: ()
-          ( or ): ()
-          B: {}
-          { or }: {}
-          < or >: <>
-          [ or ]: []
-          p: paragraph
-          s: sentence
-          t: tag
-          w: word
-          W: WORD
+            b: ()
+            ( or ): ()
+            B: {}
+            { or }: {}
+            < or >: <>
+            [ or ]: []
+            ', ", `
+            p: paragraph
+            s: sentence
+            t: tag
+            w: word
+            W: WORD
           ]]
           --
 
           -- stylua: ignore start
-          ["af"] = { query = "@function.outer",   desc = "@function.outer" },
-          ["if"] = { query = "@function.inner",   desc = "@function.inner" },
-          ["al"] = { query = "@loop.outer",       desc = "@loop.outer" },
-          ["il"] = { query = "@loop.inner",       desc = "@loop.inner" },
-          ["in"] = { query = "@number.inner",     desc = "@number.inner" },
-          ["im"] = { query = "@parameter.inner",  desc = "@parameter.inner" },
-          ["am"] = { query = "@parameter.outer",  desc = "@parameter.outer" },
-          --
-          ["ac"] = { query = "@class.outer",      desc = "@class.outer" },
-          ["ic"] = { query = "@class.inner",      desc = "@class.inner" },
-          --
-          ["aC"] = { query = "@call.outer",       desc = "@call.outer" },
-          ["iC"] = { query = "@call.inner",       desc = "@call.inner" },
-
-          -- go, js, tex, lua, py, rs, ts
-          ["ak"] = { query = "@block.outer",      desc = "@block.outer" },
-          ["ik"] = { query = "@block.inner",      desc = "@block.inner" },
-
           -- sh, css, fish, go, js, lua, py, rs, ts, yaml
+          ["as"] = { query = "@statement.outer",  desc = "@statement.outer" }, -- override sentence (won't override if not supported)
           ["aa"] = { query = "@assignment.outer", desc = "@assignment.outer" },
           ["ia"] = { query = "@assignment.inner", desc = "@assignment.inner" },
           ["la"] = { query = "@assignment.lhs",   desc = "@assignment.lhs" },
           ["ra"] = { query = "@assignment.rhs",   desc = "@assignment.rhs" },
 
-          -- override sentence (won't override if not supported)
-          ["as"] = { query = "@statement.outer",  desc = "@statement.outer" },
+          -- @comment.inner 는 잘 지원되지 않음.
+          ["a/"] ={ query = "@comment.outer",  desc = "@comment.outer" },
+          ["a*"] ={ query = "@comment.outer",  desc = "@comment.outer" },
+          ["ac"] ={ query = "@comment.outer",  desc = "@comment.outer" },
+          -- ["ic"] ={ query = "@comment.inner",  desc = "@comment.inner" },
+
+          ["am"] = { query = "@function.outer",   desc = "@function.outer" },
+          ["im"] = { query = "@function.inner",   desc = "@function.inner" },
+
+          ["al"] = { query = "@loop.outer",       desc = "@loop.outer" },
+          ["il"] = { query = "@loop.inner",       desc = "@loop.inner" },
+          ["in"] = { query = "@number.inner",     desc = "@number.inner" },
+          ["ip"] = { query = "@parameter.inner",  desc = "@parameter.inner" },
+          ["ap"] = { query = "@parameter.outer",  desc = "@parameter.outer" },
+          --
+          ["aC"] = { query = "@class.outer",      desc = "@class.outer" },
+          ["iC"] = { query = "@class.inner",      desc = "@class.inner" },
+          --
+          ["ax"] = { query = "@call.outer",       desc = "@call.outer" },
+          ["ix"] = { query = "@call.inner",       desc = "@call.inner" },
+
+          -- go, js, tex, lua, py, rs, ts
+          ["ak"] = { query = "@block.outer",      desc = "@block.outer" },
+          ["ik"] = { query = "@block.inner",      desc = "@block.inner" },
 
           -- stylua: ignore end
           -- ["ao"] = { query = "@conditional.outer", desc = "@conditional.outer" },
@@ -70,6 +113,7 @@ return {
       },
     },
   },
+
   main = "nvim-treesitter.configs",
   config = function(plugin, opts)
     require(plugin.main).setup(opts)
