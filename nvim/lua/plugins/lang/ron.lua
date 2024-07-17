a775e7f1562cb880f8e1b9979f663d24a83f4bc6
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-17 14:34:58 +0900

    feat: add ron language support

---
+++ nvim/lua/plugins/lang/ron.lua
@@ -0,0 +1,10 @@
+---@type LazySpec[]
+return {
+  {
+    [1] = "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function()
+      require("state.treesitter-langs"):add("ron")
+    end,
+  },
+}
