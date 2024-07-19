5622f8f2be2d909caefb78ada19081e54624ee21
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-19 18:39:13 +0900

    fix: z-enhanced 에서 탭의 윈도우를 못구했을때 대응

--- nvim/lua/config/keymaps/setups/Z-enhanced.lua
+++ nvim/lua/config/keymaps/setups/Z-enhanced.lua
@@ -1,69 +1,82 @@
 local M = {}
 
-M.setup = function()
-  local ignore_fts = {
-    -- notify = true,
-    -- noice = true,
-    -- cmp_menu = true,
-    -- cmp_docs = true,
-  }
-  local is_normal_buf = function(bufnr)
-    if vim.api.nvim_buf_get_option(bufnr, "buftype") == "" then
-      return true
-    end
-
-    return false
+local is_normal_buf = function(bufnr)
+  if vim.api.nvim_buf_get_option(bufnr, "buftype") == "" then
+    return true
   end
 
-  local zz_enhanced = function(mode)
-    local normal_bufnrs = {}
-    local abnormal_bufnrs = {}
-    local winnrs = vim.api.nvim_tabpage_list_wins(vim.fn.tabpagenr())
-    for _, winnr in ipairs(winnrs) do
-      -- not floating
-      if vim.api.nvim_win_get_config(winnr).zindex then
-        goto continue
-      end
+  return false
+end
 
-      local bufnr = vim.api.nvim_win_get_buf(winnr)
+local ignore_fts = {
+  -- notify = true,
+  -- noice = true,
+  -- cmp_menu = true,
+  -- cmp_docs = true,
+}
 
-      if ignore_fts[vim.api.nvim_buf_get_option(bufnr, "filetype")] then
-        goto continue
-      end
+local zz_enhanced = function(mode)
+  local normal_bufnrs = {}
+  local abnormal_bufnrs = {}
+  local get_winnrs = function()
+    return vim.api.nvim_tabpage_list_wins(vim.fn.tabpagenr())
+  end
 
-      if is_normal_buf(bufnr) then
-        table.insert(normal_bufnrs, bufnr)
-      else
-        table.insert(abnormal_bufnrs, bufnr)
-      end
-
-      ::continue::
-    end
-
-    if
-      is_normal_buf(0)
-      and (#normal_bufnrs == 1)
-      and (#abnormal_bufnrs ~= 0)
-    then
-      for _, bufnr in ipairs(abnormal_bufnrs) do
-        if vim.api.nvim_buf_get_option(bufnr, "filetype") == "minimap" then
-          vim.cmd("MinimapClose")
-        else
-          vim.api.nvim_buf_delete(bufnr, {})
-        end
-      end
-    end
-
-    -- if vim.api.nvim_buf_get_option(0, "filetype") == "minimap" then
-    --   vim.cmd("MinimapClose")
-    -- vim.api.nvim_set_current_win(window_id)
+  -- 탭이 하나 있을때, 아래 같은 이슈가 발생가 종종 있음 <2024-07-19>
+  -- E5108: Error executing lua [string ":lua"]:1: Invalid tabpage id: 1
+  local status, winnrs = pcall(get_winnr)
+  if not status then
     if mode == "ZZ" then
       vim.cmd("x")
     elseif mode == "ZQ" then
       vim.cmd("q!")
     end
+    return
   end
 
+  -- local winnrs =
+  for _, winnr in ipairs(winnrs) do
+    -- not floating
+    if vim.api.nvim_win_get_config(winnr).zindex then
+      goto continue
+    end
+
+    local bufnr = vim.api.nvim_win_get_buf(winnr)
+
+    if ignore_fts[vim.api.nvim_buf_get_option(bufnr, "filetype")] then
+      goto continue
+    end
+
+    if is_normal_buf(bufnr) then
+      table.insert(normal_bufnrs, bufnr)
+    else
+      table.insert(abnormal_bufnrs, bufnr)
+    end
+
+    ::continue::
+  end
+
+  if is_normal_buf(0) and (#normal_bufnrs == 1) and (#abnormal_bufnrs ~= 0) then
+    for _, bufnr in ipairs(abnormal_bufnrs) do
+      if vim.api.nvim_buf_get_option(bufnr, "filetype") == "minimap" then
+        vim.cmd("MinimapClose")
+      else
+        vim.api.nvim_buf_delete(bufnr, {})
+      end
+    end
+  end
+
+  -- if vim.api.nvim_buf_get_option(0, "filetype") == "minimap" then
+  --   vim.cmd("MinimapClose")
+  -- vim.api.nvim_set_current_win(window_id)
+  if mode == "ZZ" then
+    vim.cmd("x")
+  elseif mode == "ZQ" then
+    vim.cmd("q!")
+  end
+end
+
+M.setup = function()
   vim.keymap.set("n", "ZZ", function()
     zz_enhanced("ZZ")
   end, { desc = "ZZ-enhanced" })
