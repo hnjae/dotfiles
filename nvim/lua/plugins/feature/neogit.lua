0a3f055f1463e03129ac88ecfd769fcfd5b84279
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-07-26 02:08:56 +0900

    add neogit plugin spec file (not enabled)
    Sometimes there are issues with Fugitive, so I'm trying it out as a test.

---
+++ nvim/lua/plugins/feature/neogit.lua
@@ -0,0 +1,57 @@
+-- NOTE: which-key를 무시하고 고유 키 동작 가이드가 있는데, 매우 불편.  <2024-01-04>
+-- callback을 이용해서 ZZ 를 할당하고자 했으나 동작하지 않음.
+return {
+  [1] = "TimUntersberger/neogit",
+  enabled = false,
+  lazy = true,
+  cmd = {
+    "Neogit",
+    "NeogitResetState",
+  },
+  opts = {
+    mappings = {
+
+      popup = {
+        ["?"] = "HelpPopup",
+        ["A"] = "CherryPickPopup",
+        ["D"] = "DiffPopup",
+        ["M"] = "RemotePopup",
+        ["P"] = "PushPopup",
+        ["X"] = "ResetPopup",
+        ["Z"] = "StashPopup",
+        ["b"] = false, -- "BranchPopup",
+        ["B"] = "BisectPopup",
+        ["c"] = "CommitPopup",
+        ["f"] = "FetchPopup",
+        ["l"] = "LogPopup",
+        ["m"] = "MergePopup",
+        ["p"] = "PullPopup",
+        ["r"] = "RebasePopup",
+        ["v"] = "RevertPopup",
+        ["w"] = false, -- "WorktreePopup",
+      },
+    },
+  },
+  config = function(_, opts)
+    require("neogit").setup(opts)
+
+    vim.api.nvim_create_autocmd({ "FileType" }, {
+      pattern = { "NeogitStatus", "NeogitPopUp" },
+      callback = function()
+        vim.keymap.set({ "n" }, "ZZ", "<cmd>quit<CR>", { desc = "quit-neogit" })
+      end,
+    })
+  end,
+  specs = {
+    [1] = "nvim-lualine/lualine.nvim",
+    optional = true,
+    opts = function()
+      local icons = require("val").icons
+      require("state.lualine-ft-data"):add({
+        NeogitStatus = { [1] = "NeogitStatus", [2] = icons.git },
+        -- NeogitWorkTreePopup = { [1] = "NeogitStatus", [2] = icons.git },
+        -- NeogitCommitMessage = { [1] = "NeogitStatus", [2] = icons.git },
+      })
+    end,
+  },
+}
