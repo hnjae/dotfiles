dc00e2250ecec61c9de014c09aa0bb0aabb55b35
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-05 23:26:54 +0900

    mod(nix): use autoindent in nix

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -3,5 +3,5 @@ vim.opt_local.textwidth = 80
 -- indent
 -- NOTE: treesitter of nix does not support indent <2024-04-25>
 vim.opt_local.cindent = false
-vim.opt_local.autoindent = false -- 여러줄 string 선언할때 필요
+vim.opt_local.autoindent = true -- 여러줄 string 선언할때 필요
 vim.opt_local.smartindent = false
