ed0714b690b5c9c5b6ebe13252c2b898ff003987
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-08-06 19:41:51 +0900

    refactor(chatgpt): make lazykeys a function

--- nvim/lua/plugins/feature/chatgpt.lua
+++ nvim/lua/plugins/feature/chatgpt.lua
@@ -1,34 +1,12 @@
 local map_keyword = require("val.map-keyword")
 local val = require("val")
--- local prefix = require("val.prefix")
 
 local use_freedesktop_secret_service = vim.fn.executable("secret-tool") == 1
-
--- TODO: does this working? <2024-07-26>
-local check_visual_and_run_cmd = function(cmd)
-  local bufnr = vim.fn.bufnr()
-
-  -- local ESC_FEEDKEY = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
-  -- vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
-  -- vim.api.nvim_feedkeys("gv", "x", false)
-  -- vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
-
-  local start_row, start_col = unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
-  local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
-
-  if start_row == 0 or (start_row == end_row and start_col == end_col) then
-    local msg = "No visual selected: "
-      .. vim.inspect({ start_row, start_col, end_row, end_col })
-    vim.notify(msg, vim.log.levels.WARN)
-    return
-  end
-
-  vim.api.nvim_command(cmd)
-end
 
 ---@type LazySpec
 return {
   [1] = "jackMort/ChatGPT.nvim",
+  enabled = use_freedesktop_secret_service,
   main = "chatgpt",
   dependencies = {
     "MunifTanjim/nui.nvim",
@@ -37,7 +15,6 @@ return {
     "nvim-telescope/telescope.nvim",
   },
   lazy = true,
-  enabled = use_freedesktop_secret_service,
   cmd = {
     "ChatGPT",
     "ChatGPTRun",
@@ -65,14 +42,11 @@ return {
       },
       popup_input = {},
       chat = {
-        -- border_left_sign = "",
-        -- border_right_sign = "",
         max_line_length = 100,
         answer_sign = val.icons.ai,
         sessions_window = {
           active_sign = "󰄵", -- nf-md-checkbox_marked_outline
           in_active_sign = "󰄱", -- nf-md-checkbox_blank_outline
-          -- current_line_sign = "",
         },
       },
       predefined_chat_gpt_prompts = string.format(
@@ -86,8 +60,30 @@ return {
       },
     }
   end,
-  ---@type LazyKeysSpec[]
-  keys = {
+  keys = function()
+    -- TODO: does this working? <2024-07-26>
+    local check_visual_and_run_cmd = function(cmd)
+      local bufnr = vim.fn.bufnr()
+
+      -- local ESC_FEEDKEY = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+      -- vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
+      -- vim.api.nvim_feedkeys("gv", "x", false)
+      -- vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
+
+      local start_row, start_col = unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
+      local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
+
+      if start_row == 0 or (start_row == end_row and start_col == end_col) then
+        local msg = "No visual selected: "
+          .. vim.inspect({ start_row, start_col, end_row, end_col })
+        vim.notify(msg, vim.log.levels.WARN)
+        return
+      end
+
+      vim.api.nvim_command(cmd)
+    end
+
+    return {
     -- stylua: ignore start
     { [1] = "<Leader>" .. map_keyword.ai, [2] = "<cmd>ChatGPT<CR>", desc = "chatgpt-toggle" },
     { [1] = "<Leader>" .. string.upper(map_keyword.ai), [2] = "<cmd>ChatGPTActAs<CR>", desc = "chatgpt-actas" },
@@ -103,97 +99,98 @@ return {
     { [1] = "<LocalLeader>" .. map_keyword.ai .. "r", [2] = "<cmd>ChatGPTRun roxygen_edit<CR>",              desc = "roxygen-edit",             mode = { "n", "v" } },
     { [1] = "<LocalLeader>" .. map_keyword.ai .. "d", [2] = "<cmd>ChatGPTRun docstring<CR>",                 desc = "docstring",                mode = { "n", "v" } },
     { [1] = "<LocalLeader>" .. map_keyword.ai .. "x", [2] = "<cmd>ChatGPTRun explain_code<CR>",              desc = "explain-code",             mode = { "n", "v" } },
-    -- stylua: ignore end
-    {
-      [1] = "<LocalLeader>" .. map_keyword.ai .. "t",
-      [2] = function()
-        local winid = vim.fn.win_getid()
-        local bufnr = vim.fn.bufnr()
+      -- stylua: ignore end
+      {
+        [1] = "<LocalLeader>" .. map_keyword.ai .. "t",
+        [2] = function()
+          local winid = vim.fn.win_getid()
+          local bufnr = vim.fn.bufnr()
 
-        local ESC_FEEDKEY =
-          vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
+          local ESC_FEEDKEY =
+            vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
 
-        vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
-        vim.api.nvim_feedkeys("gv", "x", false)
-        vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
+          vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
+          vim.api.nvim_feedkeys("gv", "x", false)
+          vim.api.nvim_feedkeys(ESC_FEEDKEY, "n", true)
 
-        local start_row, start_col =
-          unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
-        local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
+          local start_row, start_col =
+            unpack(vim.api.nvim_buf_get_mark(bufnr, "<"))
+          local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, ">"))
 
-        if
-          start_row == 0
-          or (start_row == end_row and start_col == end_col)
-        then
-          local msg = "No visual selected: "
-            .. vim.inspect({ start_row, start_col, end_row, end_col })
-          vim.notify(msg, vim.log.levels.WARN)
-          return
-        end
-
-        vim.ui.select({
-          "English",
-          "Korean",
-          "Japanese",
-          "Chinese",
-        }, {
-          prompt = "Select language",
-        }, function(lang)
-          if not lang then
+          if
+            start_row == 0
+            or (start_row == end_row and start_col == end_col)
+          then
+            local msg = "No visual selected: "
+              .. vim.inspect({ start_row, start_col, end_row, end_col })
+            vim.notify(msg, vim.log.levels.WARN)
             return
           end
 
-          -- 이렇게 작성하면 버퍼 전체 전달
-          --   vim.api.nvim_cmd({
-          --     cmd = "ChatGPTRun",
-          --     args = { "translate", lang },
-          --     range = { cursorline },
-          --   }, { output = false })
-          -- or
-          -- require("chatgpt").run_action({
-          --   fargs = { "translate", lang },
-          -- })
+          vim.ui.select({
+            "English",
+            "Korean",
+            "Japanese",
+            "Chinese",
+          }, {
+            prompt = "Select language",
+          }, function(lang)
+            if not lang then
+              return
+            end
 
-          -- vim.api.nvim_cmd({
-          --   cmd = "ChatGPTRun",
-          --   args = { "translate", lang },
-          --   range = { start_row, end_row, },
-          -- }, { output = false })
+            -- 이렇게 작성하면 버퍼 전체 전달
+            --   vim.api.nvim_cmd({
+            --     cmd = "ChatGPTRun",
+            --     args = { "translate", lang },
+            --     range = { cursorline },
+            --   }, { output = false })
+            -- or
+            -- require("chatgpt").run_action({
+            --   fargs = { "translate", lang },
+            -- })
 
-          vim.api.nvim_set_current_win(winid)
-          vim.api.nvim_command(string.format("ChatGPTRun translate %s", lang))
-        end)
-      end,
-      desc = "translate",
-      mode = { "v" },
-    },
-    {
-      [1] = "<LocalLeader>" .. map_keyword.ai .. "g",
-      [2] = function()
-        check_visual_and_run_cmd("ChatGPTRun grammar_correction")
-      end,
-      desc = "grammer-correction",
-      mode = { "v" },
-    },
-    -- my actions
-    {
-      [1] = "<LocalLeader>" .. map_keyword.ai .. "v",
-      [2] = function()
-        check_visual_and_run_cmd("ChatGPTRun generate_variable_name")
-      end,
-      desc = "generate_variable_name",
-      mode = { "v" },
-    },
-    {
-      [1] = "<LocalLeader>" .. map_keyword.ai .. "c",
-      -- [2] = "<cmd>ChatGPTRun edit_commit_message<CR>",
-      [2] = function()
-        check_visual_and_run_cmd("ChatGPTRun edit_commit_message")
-      end,
-      desc = "edit_commit_message",
-      mode = { "n", "v" },
-    },
-  },
+            -- vim.api.nvim_cmd({
+            --   cmd = "ChatGPTRun",
+            --   args = { "translate", lang },
+            --   range = { start_row, end_row, },
+            -- }, { output = false })
+
+            vim.api.nvim_set_current_win(winid)
+            vim.api.nvim_command(string.format("ChatGPTRun translate %s", lang))
+          end)
+        end,
+        desc = "translate",
+        mode = { "v" },
+      },
+      {
+        [1] = "<LocalLeader>" .. map_keyword.ai .. "g",
+        [2] = function()
+          check_visual_and_run_cmd("ChatGPTRun grammar_correction")
+        end,
+        desc = "grammer-correction",
+        mode = { "v" },
+      },
+      -- my actions
+      {
+        [1] = "<LocalLeader>" .. map_keyword.ai .. "v",
+        [2] = function()
+          check_visual_and_run_cmd("ChatGPTRun generate_variable_name")
+        end,
+        desc = "generate_variable_name",
+        mode = { "v" },
+      },
+      {
+        [1] = "<LocalLeader>" .. map_keyword.ai .. "c",
+        -- [2] = "<cmd>ChatGPTRun edit_commit_message<CR>",
+        [2] = function()
+          check_visual_and_run_cmd("ChatGPTRun edit_commit_message")
+        end,
+        desc = "edit_commit_message",
+        mode = { "n", "v" },
+      },
+    }
+  end,
   specs = {
     {
       [1] = "folke/which-key.nvim",
