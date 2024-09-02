93e172b0d7200e6146363711ee7d6c74534826e8
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2024-09-02 19:40:48 +0900

    fix(direnv.vim): 일부 커맨드 제거한게 오류 발생해서 수정

--- nvim/lua/plugins/lang/direnv.lua
+++ nvim/lua/plugins/lang/direnv.lua
@@ -4,15 +4,15 @@ return {
     [1] = "direnv/direnv.vim",
     lazy = false,
     enabled = true,
-    -- use syntax support only
     init = function()
       vim.g.direnv_auto = 0
       vim.g.direnv_silent_load = 0
     end,
     config = function()
-      for _, command in ipairs({ "DirenvExport", "EditDirenvrc", "EditEnvrc" }) do
-        vim.api.nvim_del_user_command(command)
-      end
+      -- these could not be deleted
+      -- for _, command in ipairs({ "DirenvExport", "EditDirenvrc", "EditEnvrc" }) do
+      --   vim.api.nvim_del_user_command(command)
+      -- end
     end,
   },
 }
