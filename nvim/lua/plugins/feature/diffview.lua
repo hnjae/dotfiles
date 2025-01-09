117567b066f4a6b37f1db0d6a4c62905a35f8332
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-01-09 17:43:57 +0900

    feat(diffview): add diffview.nvim plugin configuration

---
+++ nvim/lua/plugins/feature/diffview.lua
@@ -0,0 +1,36 @@
+local utils = require("utils")
+local icons = require("val").icons
+
+return {
+  [1] = "sindrets/diffview.nvim",
+  enabled = true,
+  lazy = true,
+  cmd = "DiffviewFileHistory",
+  opts = {
+    use_icons = utils.enable_icon,
+    icons = { -- Only applies when use_icons is true.
+      folder_closed = "", --nf-cod-folder
+      folder_open = "", --nf-cod-folder_opened
+    },
+    signs = {
+      -- fold_closed = "",
+      -- fold_open = "",
+      -- fold_closed = "󰞘", -- nf-md-expand-right
+      -- fold_open = "󰞖", -- nf-md-arrow-expand-down
+      -- fold_closed = " ", -- nf-oct-fold
+      -- fold_open = " ", -- nf-oct-fold_down
+      done = icons.check,
+    },
+  },
+  specs = {
+    [1] = "nvim-lualine/lualine.nvim",
+    optional = true,
+    opts = function()
+      require("state.lualine-ft-data"):add({
+        DiffviewFileHistory = {
+          icon = "", -- nf-cod-diff
+        },
+      })
+    end,
+  },
+}
