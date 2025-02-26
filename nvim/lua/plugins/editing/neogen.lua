7547da71d8ef3eb2e9bbc85e39469be1333b553c
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-02-26 17:32:55 +0900

    feat(neogen): configure neogen

--- nvim/lua/plugins/editing/neogen.lua
+++ nvim/lua/plugins/editing/neogen.lua
@@ -1,9 +1,11 @@
 -- annotation generator
 
+local prefix = "<LocalLeader>n"
+
 ---@type LazySpec
 return {
   [1] = "danymat/neogen",
-  lazy = false,
+  lazy = true,
   enabled = require("utils").is_treesitter,
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
@@ -11,6 +13,103 @@ return {
   cmd = {
     "Neogen",
   },
-  opts = {},
+  opts = {
+    languages = {
+      lua = {
+        template = {
+          annotation_convention = "emmylua",
+        },
+      },
+      python = {
+        template = {
+          annotation_convention = "reST",
+        },
+      },
+      rust = {
+        template = {
+          annotation_convention = "rustdoc",
+        },
+      },
+      typescript = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+      typescriptreact = {
+        template = {
+          annotation_convention = "tsdoc",
+        },
+      },
+    },
+    snippet_engine = "luasnip",
+  },
+  keys = function()
+    return {
+      {
+        [1] = prefix .. "c",
+        [2] = function()
+          require("neogen").generate({
+            type = "class",
+          })
+        end,
+        desc = "neogen-class",
+      },
+      {
+        [1] = prefix .. "m",
+        [2] = function()
+          require("neogen").generate({
+            type = "func",
+          })
+        end,
+        desc = "neogen-fun",
+      },
+      {
+        [1] = prefix .. "t",
+        [2] = function()
+          require("neogen").generate({
+            type = "type",
+          })
+        end,
+        desc = "neogen-type",
+      },
+      {
+        [1] = prefix .. "f",
+        [2] = function()
+          require("neogen").generate({
+            type = "file",
+          })
+        end,
+        desc = "neogen-file",
+      },
+    }
+  end,
   config = true,
+  specs = {
+    {
+      [1] = "folke/which-key.nvim",
+      optional = true,
+      opts = function(_, opts)
+        opts.icons = opts.icons or {}
+        opts.icons.rules = opts.icons.rules or {}
+        opts.spec = opts.spec or {}
+
+        -- vim.list_extend(opts.icons.rules, {
+        --   {
+        --     pattern = "neogen",
+        --     icon = require("val.icons").ai,
+        --     color = "※",
+        --   },
+        --[[ -- })
+]]
+        vim.list_extend(opts.spec, {
+          {
+            [1] = prefix,
+            group = "neogen",
+            ---@type wk.Icon
+            icon = "󰏩", -- nf-md-paw
+          },
+        })
+      end,
+    },
+  },
 }
