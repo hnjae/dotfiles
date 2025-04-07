b17ebe6a7ce8524e01de58c943b82db5d4cf5755
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-04-07 18:46:14 +0900

    feat(fzf-lua): replace live_grep

--- nvim/lua/plugins/builtins/fzf-lua.lua
+++ nvim/lua/plugins/builtins/fzf-lua.lua
@@ -1,9 +1,37 @@
+-- copied from FzfLua' M.grep.grep_project
+local default_grep_opts = {
+  search = "",
+  fzf_opts = {
+    ["--delimiter"] = ":",
+    ["--nth"] = "3..",
+  },
+}
+
+local grep_project = function()
+  local opts = vim.deepcopy(default_grep_opts)
+  opts.search_paths = { LazyVim.root() }
+
+  require("fzf-lua").grep(opts)
+end
+
+local grep_cwd = function()
+  local opts = vim.deepcopy(default_grep_opts)
+  opts.search_paths = { vim.fn.getcwd() }
+
+  require("fzf-lua").grep(opts)
+end
+
 ---@type LazySpec
 return {
   [1] = "ibhagwan/fzf-lua",
   optional = true,
   keys = {
     { [1] = "<F1>", [2] = "<cmd>FzfLua helptags<CR>", desc = "help-tags" },
+
+    -- default mapping does not uses fzf's syntax
+    { [1] = "<Leader>/", [2] = grep_project, desc = "Grep (Project)" },
+    { [1] = "<Leader>sg", [2] = grep_project, desc = "Grep (Project)" }, -- same as above
+    { [1] = "<Leader>sG", [2] = grep_cwd, desc = "Grep (CWD)" },
   },
   opts = function()
     local fzf = require("fzf-lua")
