32e2435995e7cf71065ffa5157ba4e67ec2c778e
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-04-07 21:13:12 +0900

    feat: add hurl

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,8 +19,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "plugins" },
     { import = "plugins.builtins" },
+    { import = "plugins" },
+    { import = "plugins.lang" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
