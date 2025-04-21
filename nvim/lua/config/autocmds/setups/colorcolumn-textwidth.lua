8008aaced3afd945a58229049c059b9ae9328ecb
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-04-21 15:26:25 +0900

    feat: adjust colorcolumn on neovim window resize

--- nvim/lua/config/autocmds/setups/colorcolumn-textwidth.lua
+++ nvim/lua/config/autocmds/setups/colorcolumn-textwidth.lua
@@ -3,21 +3,68 @@ local M = {}
 -- NOTE: MAX_COLORCOLUMN 이 커지면 vim이 느려짐. <2023-12-11>
 local MAX_COLORCOLUMN = 300
 
-local set_colorcolumn = function(obj)
-  if not vim.bo[obj.buf].buflisted then
+--[[
+
+Examples of event:
+
+```lua
+{
+  buf = 1,
+  event = "BufWinEnter",
+  file = "/home/hnjae/Projects/dotfiles/nvim/lua/config/autocmds/setups/colorcolumn-textwidth.lua",
+  group = 22,
+  id = 96,
+  match = "/home/hnjae/Projects/dotfiles/nvim/lua/config/autocmds/setups/colorcolumn-textwidth.lua"
+}
+```
+
+```lua
+{
+  buf = 1,
+  event = "WinResized",
+  file = "1000",
+  group = 22,
+  id = 98,
+  match = "1000"
+}
+``
+
+```lua
+{
+  buf = 0,
+  event = "OptionSet",
+  file = "",
+  group = 22,
+  id = 97,
+  match = "textwidth"
+}
+```
+
+]]
+
+---@param event vim.api.keyset.create_autocmd.callback_args
+local set_colorcolumn = function(event)
+  if not vim.bo[event.buf].buflisted then
     return
   end
 
-  -- skip gitcommit filetype
-  if vim.opt_local.filetype:get() == "gitcommit" then
+  -- skip gitcommit, filetype
+  if vim.api.nvim_get_option_value("filetype", { buf = event.buf }) == "gitcommit" then
     return
   end
 
-  local textwidth = vim.opt_local.textwidth:get()
-  if not textwidth or textwidth < 1 or (textwidth + 1) > MAX_COLORCOLUMN then
-    if vim.opt_local.colorcolumn ~= "" then
-      vim.opt_local.colorcolumn = ""
-    end
+  local textwidth = vim.api.nvim_get_option_value("textwidth", { buf = event.buf })
+  local winid = vim.fn.bufwinid(event.buf)
+  if
+    not textwidth
+    or textwidth < 1
+    or (textwidth + 1) > MAX_COLORCOLUMN
+    or vim.fn.winwidth(winid)
+        - vim.api.nvim_get_option_value("numberwidth", { win = winid })
+        - 2
+      < textwidth -- 2 = signcolumn + foldcolumn
+  then
+    vim.opt_local.colorcolumn = ""
 
     return
   end
@@ -43,6 +90,7 @@ M.setup = function()
     { "OptionSet" },
     { pattern = { "textwidth" }, group = au_id, callback = set_colorcolumn }
   )
+  vim.api.nvim_create_autocmd({ "WinResized" }, { group = au_id, callback = set_colorcolumn })
 end
 
 return M
