84fd23cce8e7a4d1785d85200af35fee5839248a
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2025-09-14 23:21:50 +0900

    style: run yamlfix formatter

--- profiles/00-default/xdg-config/nvim/lua/plugins/lang/yaml.formatting.lua
+++ profiles/00-default/xdg-config/nvim/lua/plugins/lang/yaml.formatting.lua
@@ -4,14 +4,17 @@ return {
   optional = true,
   opts = {
     formatters_by_ft = {
-      yaml = { "yamlfmt" },
+      -- yaml = { "yamlfmt" },
+      -- yaml = { "yq" },
+      yaml = { "yamlfix" },
     },
   },
   specs = {
     {
       [1] = "mason.nvim",
       optional = true,
-      opts = { ensure_installed = { "yamlfmt" } },
+      -- opts = { ensure_installed = { "yamlfmt" } },
+      opts = { ensure_installed = { "yamlfix" } },
     },
   },
 }
