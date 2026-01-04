82476b2945f9a7db1c275a65d8c459fb266d295a
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-01-04 18:17:28 +0900

    osiris: 2026-01-04T18:17:28+09:00

--- profiles/00-default/xdg-config/nvim/lua/plugins/overrides/_not-in-use/ai.copilot.lua
+++ profiles/00-default/xdg-config/nvim/lua/plugins/overrides/_not-in-use/ai.copilot.lua
@@ -1,156 +1,136 @@
---[[
-  NOTE:
-    - My own implementation of `ai.copilot` from LazyVim
-    - DO NOT USE `ai.copilot` from LazyVim
-]]
-
----@type LazySpec[]
+---@type LazySpec
 return {
-  {
-    [1] = "zbirenbaum/copilot.lua",
-    version = false,
-    build = ":Copilot auth",
-
-    cond = not vim.g.vscode and vim.fn.executable("node") == 1,
-
-    lazy = true,
-    cmd = "Copilot",
-    event = "BufReadPost",
-
-    --[[ keys = function()
-      local prefix = require("globals").prefix
-      local keyword = "c"
-      return {
-        {
-          [1] = prefix.sidebar .. keyword,
-          [2] = "<cmd>Copilot panel<CR>",
-          desc = "copilot-panel",
-        },
-        {
-          [1] = prefix.close .. keyword,
-          [2] = "<cmd>Copilot disable<CR>",
-          desc = "copilot-disable",
-        },
-      }
-    end, ]]
-
-    keys = {
-      -- <Leader>ua default: Toggle Animation
-      -- TODO: 이거 안먹는데?? <2025-05-01>
-      -- Snacks.toggle.animate():map("<leader>ua") 가 먹고 있어서 그런가.
-      {
-        [1] = "<leader>ua",
-        [2] = "<cmd>Copilot toggle<CR>",
-        desc = "copilot-toggle",
-      },
+  [1] = "copilot.lua",
+  optional = true,
+  enabled = true,
+  keys = {
+    -- <Leader>ua default: Toggle Animation
+    -- TODO: 이거 안먹는데?? <2025-05-01>
+    -- Snacks.toggle.animate():map("<leader>ua") 가 먹고 있어서 그런가.
+    {
+      [1] = "<leader>ua",
+      [2] = "<cmd>Copilot toggle<CR>",
+      desc = "copilot-toggle",
     },
-    opts = {
-      panel = {
-        -- enabled = false,
-        auto_refresh = true,
-      },
-      suggestion = {
-        -- enabled = false,
-        auto_trigger = true,
-        hide_during_completion = true,
-        debounce = 100, -- default 75
-        keymap = {
-          accept = "<F10>",
-          -- accept_word = false,
-          -- accept_line = false,
-          -- next = "<M-]>",
-          -- prev = "<M-[>",
-          -- dismiss = "<C-]>",
-          dismiss = "<S-F10>",
-        },
-      },
-      filetypes = {
-        sh = function()
-          if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-            -- Disable for `.env` files
-            return false
-          end
-          return true
-        end,
+  },
+  opts = {
+    filetypes = {
+      sh = function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        local basename = vim.fs.basename(bufname)
 
-        -- data-formats
-        yaml = false,
-        json = false,
-        jsonc = false,
-        toml = false,
-        ini = false,
+        -- Disable for `.env` files
+        if string.match(basename, "^%.env.*") or string.match(basename, "env") then
+          return false
+        elseif vim.startswith(bufname, "/tmp/") then
+          return false
+        end
 
-        -- docs
-        text = false,
-        markdown = false,
-        org = false,
-        norg = false,
-        asciidoctor = false,
-        asciidoc = false,
-        rst = false,
-        help = false,
-        [""] = false,
+        return true
+      end,
 
-        -- version-control
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
+      nix = function()
+        if string.match(vim.api.nvim_buf_get_name(0), "^.*-encrypted.nix") then
+          return false
+        end
+        return true
+      end,
 
-        --
-        c = true,
-        rust = true,
-        java = true,
-        lua = true,
-        python = true,
-        typescript = true,
-        javascript = true,
-        ["*"] = true,
-      },
+      -- data-formats
+      conf = false,
+      csv = false,
+      ini = false,
+      json = false,
+      jsonc = false,
+      toml = false,
+      xml = false,
+      yaml = false,
+
+      -- plain text accounting (ledger)
+      ledger = false,
+      beancount = false,
+
+      -- docs
+      text = false,
+      markdown = false,
+      org = false,
+      norg = false,
+      asciidoctor = false,
+      asciidoc = false,
+      rst = false,
+      help = false,
+      [""] = false,
+      typst = false,
+      tex = false,
+
+      -- version-control
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+
+      --
+      c = true,
+      rust = true,
+      java = true,
+      lua = true,
+      python = true,
+      typescript = true,
+      javascript = true,
+      ["yaml.ansible"] = true,
+      ["*"] = true,
     },
-    specs = {
-      {
-        [1] = "lualine.nvim",
-        optional = true,
-        dependencies = { "AndreM222/copilot-lualine" },
-        opts = function(_, opts)
-          opts.sections = opts.sections or {}
-          opts.sections.lualine_x = opts.sections.lualine_x or {}
-
-          local colors = {
-            winbar = require("copilot-lualine.colors").get_hl_value(0, "Winbar", "fg"),
-            warn = require("copilot-lualine.colors").get_hl_value(0, "DiagnosticWarn", "fg"),
-            error = require("copilot-lualine.colors").get_hl_value(0, "DiagnosticError", "fg"),
-          }
-
-          table.insert(opts.sections.lualine_x, {
-            [1] = "copilot",
-            -- show_colors = true,
-            symbols = {
-              spinner_color = colors.winbar,
-              status = {
-                icons = {
-                  -- enabled = " ", --nf-oct-copilot
-                  -- sleep = " ", -- auto-trigger disabled
-                  -- disabled = " ",
-                  -- warning = " ",
-
-                  -- NOTE: Use nf-oct variant to match icon size of above elements <2025-01-02>
-                  unknown = " ", -- nf-oct-skip
-                },
-                hl = {
-                  enabled = colors.winbar,
-                  sleep = colors.winbar,
-                  disabled = colors.warn,
-                  warning = colors.error,
-                  unknown = colors.error,
-                },
-              },
-            },
-          })
-        end,
-      },
-    },
+  },
+  specs = {
+    -- {
+    --   [1] = "lualine.nvim",
+    --   optional = true,
+    --   dependencies = { "copilot-lualine" },
+    --   opts = function(_, opts)
+    --     opts.sections = opts.sections or {}
+    --     opts.sections.lualine_x = opts.sections.lualine_x or {}
+    --
+    --     local colors = {
+    --       winbar = require("copilot-lualine.colors").get_hl_value(0, "Winbar", "fg"),
+    --       warn = require("copilot-lualine.colors").get_hl_value(0, "DiagnosticWarn", "fg"),
+    --       error = require("copilot-lualine.colors").get_hl_value(0, "DiagnosticError", "fg"),
+    --     }
+    --
+    --     -- for _, component in ipairs(opts.sections.lualine_x) do
+    --     --   -- if component[1] == "copilot" then
+    --     --   -- end
+    --     -- end
+    --
+    --     -- HACK: Lazyvim 14.14.0 기준 , 기존 copliot 아이콘 제거
+    --     table.remove(opts.sections.lualine_x, 2) --remove
+    --
+    --     table.insert(opts.sections.lualine_x, {
+    --       [1] = "copilot",
+    --       -- show_colors = true,
+    --       symbols = {
+    --         spinner_color = colors.winbar,
+    --         status = {
+    --           icons = {
+    --             -- enabled = " ", --nf-oct-copilot
+    --             -- sleep = " ", -- auto-trigger disabled
+    --             -- disabled = " ",
+    --             -- warning = " ",
+    --
+    --             -- NOTE: Use nf-oct variant to match icon size of above elements <2025-01-02>
+    --             unknown = " ", -- nf-oct-skip
+    --           },
+    --           hl = {
+    --             enabled = colors.winbar,
+    --             sleep = colors.winbar,
+    --             disabled = colors.warn,
+    --             warning = colors.error,
+    --             unknown = colors.error,
+    --           },
+    --         },
+    --       },
+    --     })
+    --   end,
+    -- },
   },
 }
