04a0831dc7de3075902e5780a5da751a714ccf42
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-01-15 17:59:51 +0900

    isis: 2026-01-15T17:59:51+09:00

--- profiles/00-default/xdg-config/wezterm/wezterm.lua
+++ profiles/00-default/xdg-config/wezterm/wezterm.lua
@@ -19,8 +19,8 @@ config.animation_fps = 60 -- default: 10, visual_bell 에서 사용됨.
 -- Inactive pane dimming
 -- https://wezterm.org/config/appearance.html#styling-inactive-panes
 config.inactive_pane_hsb = {
-  saturation = 0.7, -- default: 0.9
-  brightness = 0.9, -- default: 0.8
+  saturation = 0.9, -- default: 0.9
+  brightness = 0.8, -- default: 0.8
 }
 
 require("fonts").apply_to_config(config)
