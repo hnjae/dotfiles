f4a5ab3a5947a1fc766b2895ee21e5f71ec7f522
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-03-15 11:22:59 +0900

    hemera: 2026-03-15T11:22:59+09:00

--- ansible/run.sh
+++ ansible/run.sh
@@ -6,7 +6,7 @@ scrip_dir="$(cd -- "$(dirname -- "$0")" && pwd -P)" >/dev/null 2>&1
 
 check_cond() {
     if ! command -v ansible >/dev/null 2>&1; then
-        echo "ERROR: ansible is not installed" >&2
+        echo "ERR: ansible is not installed" >&2
         exit 1
     fi
 }
