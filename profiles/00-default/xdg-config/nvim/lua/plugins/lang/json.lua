0311ad9b0d3fce222b4f6e98cf2f683bae56cd64
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-03-21 13:03:12 +0900

    hemera: 2026-03-21T13:03:12+09:00

--- profiles/00-default/xdg-config/nvim/lua/plugins/lang/json.lua
+++ profiles/00-default/xdg-config/nvim/lua/plugins/lang/json.lua
@@ -1,33 +1,27 @@
 ---@type LazySpec[]
 return {
   {
-    [1] = "nvim-lint",
+    "conform.nvim",
     optional = true,
     opts = {
-      -- linters_by_ft = {
-      --   json = { "jsonlint" },
-      -- },
-    },
-    specs = {
-      -- {
-      --   [1] = "mason.nvim",
-      --   optional = true,
-      --   opts = { ensure_installed = { "jsonlint" } },
-      -- },
-    },
-  },
-  {
-    [1] = "nvim-lspconfig",
-    optional = true,
-    -- ---@type lspconfig.options
-    opts = {
-      servers = {
-        -- use biome instaed
-        -- jsonls = {
-        --   enabled = false,
-        --   mason = false,
-        -- },
+      formatters_by_ft = {
+        json = { "biome", "jq", lsp_format = "fallback" },
       },
     },
   },
+  -- {
+  --   [1] = "nvim-lspconfig",
+  --   optional = true,
+  --   -- ---@type lspconfig.options
+  --   opts = {
+  --     servers = {
+  --       -- use biome instead
+  --       -- jsonls = {
+  --       --   enabled = false,
+  --       --   mason = false,
+  --       -- },
+
+  --     },
+  --   },
+  -- },
 }
