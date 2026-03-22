ce38102fd8605f8a1ca20e6fb0148b10742c8793
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-03-22 21:56:43 +0900

    nyx: 2026-03-22T21:56:42+09:00

--- profiles/00-default/xdg-config/zsh/my-bat/init.zsh
+++ profiles/00-default/xdg-config/zsh/my-bat/init.zsh
@@ -8,12 +8,4 @@ fi
 
 export BAT_PAGER="less -iLR"
 
-if [[ $TERM_PROGRAM == "WezTerm" ]]; then
-    export BAT_THEME="base16"
-elif [[ $TERM_PROGRAM == "ghostty" ]]; then
-    export BAT_THEME="base16-256"
-else
-    export BAT_THEME="ansi"
-fi
-
 alias -- c="bat --paging=never --style=plane"
