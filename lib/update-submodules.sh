ed2cce5cdda0449ec2d33d1a063cd4333ede62f8
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-04-17 13:14:29 +0900

    feat(update-submodules): handle branch-based submodules

--- lib/update-submodules.sh
+++ lib/update-submodules.sh
@@ -4,6 +4,12 @@ set -euo pipefail
 
 script_dir="$(cd -- "$(dirname -- "$0")" && pwd -P)" >/dev/null 2>&1
 
+declare -A BRANCH_BASED_MODULES=(
+    [profiles/desktop/xdg-config-home/mpv/script-repos/thumbfast]=1
+    [profiles/desktop/xdg-config-home/mpv/script-repos/osc.lua]=1
+    [profiles/desktop/xdg-config-home/mpv/script-repos/content-red-tester-s-scripts]=1
+)
+
 update_module_by_tag() {
     local module="$1"
     local latest_tag
@@ -29,7 +35,7 @@ update_module_by_tag() {
     latest_tag="$(git -C "$module" tag -l --sort=-version:refname | head -n1)"
 
     if [ "$latest_tag" = "" ]; then
-        echo "ERROR: No tags found in $module" >&2
+        echo "ERR: No tags found in $module" >&2
         return 1
     fi
 
@@ -51,16 +57,15 @@ update_module_by_tag() {
     fi
 }
 
-#!!!!!!!!!!!!!!!!!!!!!!!
-# HAVE NOT TESTED THIS
-#!!!!!!!!!!!!!!!!!!!!!!!
 update_module_by_default_branch() {
     local module="$1"
+    local default_branch
+    local current_commit
+    local target_commit
+
     if [ ! -d "$module" ]; then
         echo "INFO: $module not found, initializing submodule" >&2
         git submodule update --init --recursive "$module"
-        # This will fetch main/master branch
-        return 0
     fi
 
     if ! git -C "$module" diff --quiet HEAD; then
@@ -76,15 +81,17 @@ update_module_by_default_branch() {
     # TODO: git fetch -v --porcelain 으로 default_branch 명 캡쳐 <2025-08-13>
 
     git -C "$module" fetch --quiet origin
-    local default_branch
     default_branch=$(git -C "$module" remote show origin | grep 'HEAD branch' | cut -d' ' -f5)
 
     if [ "$default_branch" = "" ]; then
-        echo "ERROR: No default branch found for $module" >&2
+        echo "ERR: No default branch found for $module" >&2
         return 1
     fi
 
-    if git -C "$module" diff --quiet HEAD "$latest_tag"; then
+    current_commit=$(git -C "$module" rev-parse HEAD)
+    target_commit=$(git -C "$module" rev-parse "origin/$default_branch")
+
+    if [ "$current_commit" = "$target_commit" ]; then
         echo "INFO: $module is already at the latest commit origin/$default_branch" >&2
         return 0
     fi
@@ -100,8 +107,6 @@ update_module_by_default_branch() {
 }
 
 main() {
-    declare -A branch_based_modules
-
     local modules
 
     cd "$script_dir" # Make sure pwd is not in submodule
@@ -111,8 +116,8 @@ main() {
     # shellcheck disable=SC2016
     modules="$(git submodule --quiet foreach 'echo $path' 2>/dev/null)"
     while IFS= read -r module; do
-        if [[ ${branch_based_modules[$module]:-} != "" ]]; then
-            echo "WIP: Implement this"
+        if [[ ${BRANCH_BASED_MODULES[$module]:-} != "" ]]; then
+            update_module_by_default_branch "$module"
         else
             update_module_by_tag "$module"
         fi
