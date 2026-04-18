031c39917cf475d7ec087ff3515062046cc6c5d1
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-04-18 10:09:27 +0900

    chore: typos overhaul

--- devenv.nix
+++ devenv.nix
@@ -2,6 +2,8 @@ _: {
   git-hooks.hooks = {
     rumdl.enable = true;
     yamlfmt.enable = true;
+    taplo.enable = true;
+    typos.enable = true;
     biome = {
       enable = true;
       excludes = [ "lazy-lock\\.json" ];
