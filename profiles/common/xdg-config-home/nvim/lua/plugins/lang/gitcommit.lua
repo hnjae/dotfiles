6a061fdf582c9f0eb3b4657b7dea3849786501a3
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-05-05 10:00:14 +0900

    nyx: 2026-05-05T10:00:14+09:00

--- profiles/common/xdg-config-home/nvim/lua/plugins/lang/gitcommit.lua
+++ profiles/common/xdg-config-home/nvim/lua/plugins/lang/gitcommit.lua
@@ -7,6 +7,49 @@ local function add_gitcommit_linter(opts, linter)
   end
 end
 
+local function git_root(filename)
+  local dirname = vim.fn.fnamemodify(filename, ":p:h")
+  local git_dir = vim.fs.find(".git", { path = dirname, upward = true })[1]
+
+  return git_dir ~= nil and vim.fs.dirname(git_dir) or nil
+end
+
+local function current_git_root()
+  return git_root(vim.api.nvim_buf_get_name(0)) or vim.fn.getcwd()
+end
+
+local function cocogitto_parser(output)
+  if output == nil or vim.trim(output) == "" then
+    return {}
+  end
+
+  local message = output:match("Error:%s*([^\r\n]+)")
+  if message == nil then
+    return {}
+  end
+
+  local expected = output:match("\n%s*=%s*([^\r\n]+)")
+  if expected ~= nil then
+    message = ("%s: %s"):format(message, expected)
+  end
+
+  local lnum, col = output:match("%-%->%s*(%d+):(%d+)")
+
+  return {
+    {
+      source = "cocogitto",
+      lnum = math.max((tonumber(lnum) or 1) - 1, 0),
+      col = math.max((tonumber(col) or 1) - 1, 0),
+      severity = vim.diagnostic.severity.ERROR,
+      message = message,
+    },
+  }
+end
+
+local function current_commit_message()
+  return table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, true), "\n")
+end
+
 ---@type LazySpec[]
 return {
   {
@@ -18,17 +61,6 @@ return {
     [1] = "nvim-lint",
     optional = true,
     opts = function(_, opts)
-      local function git_root(filename)
-        local dirname = vim.fn.fnamemodify(filename, ":p:h")
-        local git_dir = vim.fs.find(".git", { path = dirname, upward = true })[1]
-
-        return git_dir ~= nil and vim.fs.dirname(git_dir) or nil
-      end
-
-      local function current_git_root()
-        return git_root(vim.api.nvim_buf_get_name(0)) or vim.fn.getcwd()
-      end
-
       opts.linters = opts.linters or {}
       opts.linters.gitlint = vim.tbl_extend("force", opts.linters.gitlint or {}, {
         args = {
@@ -45,16 +77,21 @@ return {
         end,
       })
 
-      add_gitcommit_linter(opts, "gitlint")
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.gitcommit = opts.linters_by_ft.gitcommit or {}
+
+      if not vim.tbl_contains(opts.linters_by_ft.gitcommit, "gitlint") then
+        table.insert(opts.linters_by_ft.gitcommit, "gitlint")
+      end
 
       return opts
     end,
     specs = {
-      -- {
-      --   [1] = "mason.nvim",
-      --   optional = true,
-      --   opts = { ensure_installed = { "gitlint" } },
-      -- },
+      {
+        [1] = "mason.nvim",
+        optional = true,
+        opts = { ensure_installed = { "gitlint" } },
+      },
     },
   },
   {
@@ -92,7 +129,43 @@ return {
         return opts
       end
 
-      add_gitcommit_linter(opts, "commitlint")
+      opts.linters_by_ft = opts.linters_by_ft or {}
+      opts.linters_by_ft.gitcommit = opts.linters_by_ft.gitcommit or {}
+
+      if not vim.tbl_contains(opts.linters_by_ft.gitcommit, "commitlint") then
+        table.insert(opts.linters_by_ft.gitcommit, "commitlint")
+      end
+
+      return opts
+    end,
+    specs = {},
+  },
+  {
+    --[[
+      NOTE:
+        - Lint commit messages
+        - <https://docs.cocogitto.io/guide/verify.html>
+    --]]
+    [1] = "nvim-lint",
+    optional = true,
+    opts = function(_, opts)
+      opts.linters = opts.linters or {}
+      opts.linters.cocogitto = vim.tbl_extend("force", opts.linters.cocogitto or {}, {
+        cmd = "cog",
+        append_fname = false,
+        args = {
+          "verify",
+          current_commit_message,
+        },
+        stream = "stderr",
+        ignore_exitcode = true,
+        parser = cocogitto_parser,
+        condition = function(ctx)
+          return vim.fn.executable("cog") == 1 and git_root(ctx.filename) ~= nil
+        end,
+      })
+
+      add_gitcommit_linter(opts, "cocogitto")
 
       return opts
     end,
