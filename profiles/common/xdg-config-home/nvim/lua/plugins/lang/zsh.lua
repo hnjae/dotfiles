1e55139ed60810cf445e6f7c35739554633ad0f2
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-05-09 15:34:27 +0900

    feat(nvim): add shfmt formatter for zsh

--- profiles/common/xdg-config-home/nvim/lua/plugins/lang/zsh.lua
+++ profiles/common/xdg-config-home/nvim/lua/plugins/lang/zsh.lua
@@ -1,6 +1,36 @@
 ---@type LazySpec[]
 return {
   {
+    [1] = "conform.nvim",
+    optional = true,
+    opts = {
+      formatters = {
+        shfmt_zsh = {
+          command = "shfmt",
+          args = function(_, ctx)
+            local args = { "--language-dialect=zsh", "--filename", "$FILENAME" }
+
+            if vim.bo[ctx.buf].expandtab then
+              vim.list_extend(args, { "--indent", ctx.shiftwidth })
+            end
+
+            return args
+          end,
+        },
+      },
+      formatters_by_ft = {
+        zsh = { "shfmt_zsh" },
+      },
+    },
+    specs = {
+      {
+        [1] = "mason.nvim",
+        optional = true,
+        opts = { ensure_installed = { "shfmt" } },
+      },
+    },
+  },
+  {
     [1] = "nvim-lint",
     optional = true,
     opts = {
