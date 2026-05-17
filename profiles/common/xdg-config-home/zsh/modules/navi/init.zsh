ade11751edaa380512ba0c836d35a1f8a2073efa
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-05-17 19:19:08 +0900

    feat(zsh): add enhanced navi-edit command with interactive cheat search

--- profiles/common/xdg-config-home/zsh/modules/navi/init.zsh
+++ profiles/common/xdg-config-home/zsh/modules/navi/init.zsh
@@ -2,11 +2,13 @@ if ((!$+commands[navi])) || [[ $TERM == "dumb" || $options[zle] != on ]]; then
     return
 fi
 
-__CHEATS_DIR="${XDG_DATA_HOME:-${HOME}/.local/share}/navi/cheats"
-
 local initfile="${0:A:h}/_navi.zsh"
 local sigfile="${initfile}.sig"
 local sig="${commands[navi]:A}"
+
+autoload -Uz navi-edit __navi_select_enhanced
+
+alias -- ne="navi-edit"
 
 # Cache invalidation policy:
 # - `-ot` catches ordinary in-place navi updates.
@@ -23,67 +25,15 @@ local sig="${commands[navi]:A}"
 # Repeated runs show the cache check/source path is about 26x cheaper than
 # regenerating the widget script each time; single-run measurements are near
 # hyperfine noise.
-if [[
+if [[ 
     ! -e "$initfile" ||
     "$initfile" -ot "${commands[navi]}" ||
     ! -e "$sigfile" ||
-    "$(<"$sigfile")" != "$sig"
-]]; then
+    "$(<"$sigfile")" != "$sig" ]] \
+    ; then
     $commands[navi] widget zsh >|"$initfile" || return 1
     print -r -- "$sig" >|"$sigfile"
     zcompile -UR "$initfile"
 fi
 
 source "$initfile"
-
-function __navi_select() {
-    local term="$1"
-    local file
-
-    file="$(
-        (
-            fd "$term" "$__CHEATS_DIR" -e cheat -t f
-        ) |
-            sed -e "s|^$__CHEATS_DIR||g" -e "s|\.cheat$||g" |
-            fzf --select-1 --exit-0 --preview='bat -l sh --color=always --italic-text=always --paging=never --decorations=never '"$__CHEATS_DIR"'/{}.cheat' |
-            awk -v path="$__CHEATS_DIR" -v ext=".cheat" '{print path $0 ext}'
-    )"
-
-    echo "$file"
-}
-
-function navi-edit() {
-    local file
-    if [[ $1 != "" ]]; then
-        file="$__CHEATS_DIR/$1.cheat"
-    else
-        file="$(__navi_select ".")"
-        [[ $file == "" ]] && return
-    fi
-
-    nvim -- "$file"
-}
-
-function navi-show() {
-    local file
-    if [[ -n $1 && -f "$__CHEATS_DIR/$1.cheat" ]]; then
-        file="$__CHEATS_DIR/$1.cheat"
-    else
-        local term
-        if [[ $1 != "" ]]; then
-            term="$1"
-        else
-            term="."
-        fi
-
-        file="$(__navi_select "$term")"
-        [[ $file == "" ]] && return
-    fi
-
-    sed -e '/^\$/d' -- "$file" |
-        bat -l sh --color=always --decorations=never --paging=never
-}
-
-alias -- na="navi"
-alias -- ne="navi-edit"
-alias -- nsh="navi-show"
