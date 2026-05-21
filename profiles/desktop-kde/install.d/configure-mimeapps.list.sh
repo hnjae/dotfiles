bf6ead4df0466a646796f5b9cf1857584d322b63
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-05-22 01:01:16 +0900

    nyx: 2026-05-22T01:01:16+09:00

--- profiles/desktop-kde/install.d/configure-mimeapps.list.sh
+++ profiles/desktop-kde/install.d/configure-mimeapps.list.sh
@@ -3,11 +3,13 @@
 set -eu
 
 BROWSER="com.brave.Browser.desktop"
+VIDEO_PLAYER="mpv.desktop"
 
 main() {
-    handlr set 'video/mpv' io.mpv.Mpv.desktop
-    handlr set 'video/x-ms-wmv' io.mpv.Mpv.desktop
-    handlr set "video/x-ms-asf" io.mpv.Mpv.desktop
+    handlr set 'video/mp4' "$VIDEO_PLAYER"
+    handlr set 'video/x-ms-wmv' "$VIDEO_PLAYER"
+    handlr set "video/x-ms-asf" "$VIDEO_PLAYER"
+    handlr set "video/x-matroska" "$VIDEO_PLAYER"
 
     # handlr set 'audio/*' org.fooyin.fooyin.desktop
     handlr set "x-scheme-handler/http" "$BROWSER"
@@ -15,11 +17,8 @@ main() {
     handlr set "text/html" "$BROWSER"
 
     handlr set "application/vnd.oasis.opendocument.text" "org.libreoffice.LibreOffice.writer.desktop"
-
     handlr set "application/vnd.oasis.opendocument.spreadsheet" "org.libreoffice.LibreOffice.calc.desktop"
-
     handlr set "application/vnd.oasis.opendocument.presentation" "org.libreoffice.LibreOffice.impress.desktop"
-
     handlr set "application/pdf" "org.kde.okular.desktop"
 }
 
