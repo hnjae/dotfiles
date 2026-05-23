32e8ac484bf24284143377180a62a45877b22e32
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-05-23 16:10:48 +0900

    nyx: 2026-05-23T16:10:48+09:00

--- profiles/common/xdg-config-home/nvim/lua/plugins/overrides/editor.aerial.lua
+++ profiles/common/xdg-config-home/nvim/lua/plugins/overrides/editor.aerial.lua
@@ -1,7 +1,5 @@
 local icons = require("globals").icons
-local strip = function(str)
-  return string.sub(str, 1, #str - 1)
-end
+
 -- LazyVim's `editor.aerial`
 
 ---@type LazySpec[]
@@ -73,41 +71,10 @@ return {
         optional = true,
         opts = {
           filetype = {
-            aerial = { glyph = strip(icons.symbol), hl = "MiniIconsPurple" }, -- default: 󱘎
+            aerial = { glyph = icons.symbol, hl = "MiniIconsPurple" }, -- default: 󱘎
           },
         },
       },
     },
   },
-
-  -- {
-  --   [1] = "aerial.nvim",
-  --   optional = true,
-  --   opts = function()
-  --     vim.api.nvim_create_autocmd("WinClosed", {
-  --       callback = function(ev)
-  --         -- NOTE:
-  --         -- ev.buf: 닫힌 윈도우의 버퍼
-  --
-  --         if vim.api.nvim_get_option_value("buftype", { buf = ev.buf }) ~= "" then
-  --           return
-  --         end
-  --
-  --         -- HACK: 커서가 다른 윈도우에 포커스 되었다고 가정하기 위해 대기 <2025-04-22>
-  --         vim.defer_fn(function()
-  --           local buf = vim.api.nvim_get_current_buf()
-  --           if vim.api.nvim_get_option_value("filetype", { buf = buf }) ~= "aerial" then
-  --             return
-  --           end
-  --
-  --           vim.cmd("quit")
-  --
-  --           -- 아래 두개로 제거하면 안됨
-  --           -- require("snacks").bufdelete(buf)
-  --           -- require("aerial").close()
-  --         end, 5)
-  --       end,
-  --     })
-  --   end,
-  -- },
 }
