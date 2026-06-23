8276c6673e4edc988339d67133a438ae30f506d3
KIM Hyunjae <42675338+hnjae@users.noreply.github.com>
2026-06-23 12:59:35 +0900

    nyx: 2026-06-23T12:59:35+09:00

--- profiles/desktop-kde/install.d/configure-mimeapps.list.sh
+++ profiles/desktop-kde/install.d/configure-mimeapps.list.sh
@@ -15,7 +15,19 @@ main() {
     handlr set "video/vnd.avi" "$VIDEO_PLAYER"
     handlr set "video/x-msvideo" "$VIDEO_PLAYER"
 
+    # xdg-mime of image files
+    handlr set "image/apng" "$IMAGE_VIEWER"
+    handlr set "image/avif" "$IMAGE_VIEWER"
+    handlr set "image/bmp" "$IMAGE_VIEWER"
+    handlr set "image/gif" "$IMAGE_VIEWER"
+    handlr set "image/jp2" "$IMAGE_VIEWER"
     handlr set "image/jpeg" "$IMAGE_VIEWER"
+    handlr set "image/jxl" "$IMAGE_VIEWER"
+    handlr set "image/png" "$IMAGE_VIEWER"
+    handlr set "image/svg+xml" "$IMAGE_VIEWER"
+    handlr set "image/tiff" "$IMAGE_VIEWER"
+    handlr set "image/webp" "$IMAGE_VIEWER"
+    handlr set "image/x-adobe-dng" "$IMAGE_VIEWER"
 
     # handlr set 'audio/*' org.fooyin.fooyin.desktop
     handlr set "x-scheme-handler/http" "$BROWSER"
